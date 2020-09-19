f59eff68ae24dc9c4ab625200bf680f1ac30101e
YuraVolkov <yuramilkyway@gmail.com>
2020-09-19 15:31:30 +0300

    +const "fileWithMaleNames" and "fileWithFemaleNames"

--- src/ru/innopolis/main/Person.java
+++ src/ru/innopolis/main/Person.java
@@ -5,6 +5,8 @@ public class Person {
     final private int age;
     final private Sex sex;
     final private String name;
+    final private File fileWithMaleNames = new File("man.txt");
+    final private File fileWithFemaleNames = new File("woman.txt");
 
     public Person(int age, Sex sex) throws IOException {
         this.age = age;
@@ -28,12 +30,12 @@ public class Person {
         File file;
         int upperBound;
 
-        if (sex.getSex().equals("MAN")) {
-            file = new File("man.txt");
+        if (sex.thisMan(sex)) {
+            file = fileWithMaleNames;
             upperBound = (int) (Math.random() * 615);
         }
         else {
-            file = new File("woman.txt");
+            file = fileWithFemaleNames;
             upperBound = (int) (Math.random() * 725);
         }
 
