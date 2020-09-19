7f4377b4e718864364a5e3a57cd13861f1518125
YuraVolkov <yuramilkyway@gmail.com>
2020-09-19 15:30:11 +0300

    +const DEFAULT_COUNT_NAME +method launchGeneratorPerson +removing calculations from constructor

--- src/ru/innopolis/main/GeneratorPerson.java
+++ src/ru/innopolis/main/GeneratorPerson.java
@@ -3,15 +3,19 @@ package ru.innopolis.main;
 import java.io.IOException;
 
 public class GeneratorPerson {
-    final int defaultCountPerson = 10000;
+    final private int DEFAULT_COUNT_PERSON = 10000;
     final private Person[] array;
 
     public GeneratorPerson(int countPerson) throws IOException {
-        if (countPerson < defaultCountPerson) {
-            countPerson = defaultCountPerson;
-            System.out.println("Минимальное количество объектов класса Person - 10000;");
+        if (countPerson < DEFAULT_COUNT_PERSON) {
+            countPerson = DEFAULT_COUNT_PERSON;
+            System.out.println("Минимальное количество объектов класса Person: " + DEFAULT_COUNT_PERSON);
         }
         array = new Person[countPerson];
+        launchGeneratorPerson(countPerson);
+    }
+
+    private void launchGeneratorPerson(int countPerson) throws IOException {
         for (int i = 0; i < countPerson; i++) {
             int age = (int) ((Math.random() * (100)) + 1);
             Sex sex = new Sex((int) Math.round(Math.random()));
