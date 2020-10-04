ade08090bf943c5242d727be24fd3c44cb2365b8
YuraVolkov <yuramilkyway@gmail.com>
2020-10-04 22:18:49 +0300

    Добавил закрытие ресурса Scanner

--- src/ru/innopolis/main/Main.java
+++ src/ru/innopolis/main/Main.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        new CaseMenu(new Scanner(System.in)).start();
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            new CaseMenu(scanner).start();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
