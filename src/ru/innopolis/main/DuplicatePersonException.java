6f51c0d4132695d7d0d444c5f689cbe339398328
YuraVolkov <yuramilkyway@gmail.com>
2020-10-04 19:03:01 +0300

    рефактор person.toString на person

--- src/ru/innopolis/main/DuplicatePersonException.java
+++ src/ru/innopolis/main/DuplicatePersonException.java
@@ -2,6 +2,7 @@ package ru.innopolis.main;
 
 public class DuplicatePersonException extends Exception {
     public DuplicatePersonException(Person person) {
+
         super("Найден дубликат: " + person);
     }
 }
