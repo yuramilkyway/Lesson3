94728c6a25cefbab5918e30eb43bf07eb40bbf46
YuraVolkov <yuramilkyway@gmail.com>
2020-10-05 16:26:13 +0300

    Разбиение метода getName, на подметоды

--- src/ru/innopolis/library/person/GeneratorPerson.java
+++ src/ru/innopolis/library/person/GeneratorPerson.java
@@ -11,10 +11,19 @@ public class GeneratorPerson {
     private static final File FILE_WITH_FEMALE_NAMES = new File("resources/woman.txt");
     private final List<String> LIST_WITH_MEAN_NAMES = new ArrayList<>();
     private final List<String> LIST_WITH_FEMALE_NAMES = new ArrayList<>();
-    private boolean isInit = true;
+    private boolean isNotInit = true;
     final Random random = new Random();
     private final int COUNT_PERSON;
 
+    /**
+     * Конструктор класса GeneratorPerson.
+     * Задает количество элементов при генерации Person.
+     * Если принимаемое значение меньше количества,
+     * заданого по условию задачи(DEFAULT_COUNT_PERSON),
+     * то задает дефолтное(DEFAULT_COUNT_PERSON) значение элементов.
+     * @param countPerson Количество элементов, с которым мы хотим
+     *                    сгенерировать список Person.
+     */
     public GeneratorPerson(int countPerson) {
         if (countPerson < DEFAULT_COUNT_PERSON) {
             this.COUNT_PERSON = DEFAULT_COUNT_PERSON;
@@ -25,37 +34,41 @@ public class GeneratorPerson {
         }
     }
 
-    private String getName(Sex sex) {
-        try {
-            if (isInit) {
-                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_WITH_MEAN_NAMES))) {
-                    String temp = bufferedReader.readLine();
-                    while (temp != null) {
-                        LIST_WITH_MEAN_NAMES.add(temp);
-                        temp = bufferedReader.readLine();
-                    }
-                }
-                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_WITH_FEMALE_NAMES))) {
-                    String temp = bufferedReader.readLine();
-                    while (temp != null) {
-                        LIST_WITH_FEMALE_NAMES.add(temp);
-                        temp = bufferedReader.readLine();
-                    }
-                }
-                isInit = false;
-            }
+    private void initList() {
+        readNamesFromFileAndWriteToList(FILE_WITH_MEAN_NAMES, LIST_WITH_MEAN_NAMES);
+        readNamesFromFileAndWriteToList(FILE_WITH_FEMALE_NAMES, LIST_WITH_FEMALE_NAMES);
+    }
 
-            if (sex.isMan(sex)) {
-                int randomMeanName = random.nextInt(LIST_WITH_MEAN_NAMES.size());
-                return LIST_WITH_MEAN_NAMES.get(randomMeanName);
-            } else {
-                int randomFemaleName = random.nextInt(LIST_WITH_FEMALE_NAMES.size());
-                return LIST_WITH_FEMALE_NAMES.get(randomFemaleName);
+    private void readNamesFromFileAndWriteToList(File file, List<String> list) {
+        try {
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                String temp = bufferedReader.readLine();
+                while (temp != null) {
+                    list.add(temp);
+                    temp = bufferedReader.readLine();
+                }
             }
         } catch (IOException e) {
             System.out.println("Не найдены файлы для чтения имен. " + e);
         }
-        return "unnamed";
+    }
+
+    private String getNameFromList(List<String> list) {
+        int randomFemaleName = random.nextInt(list.size());
+        return list.get(randomFemaleName);
+    }
+
+    private String getName(Sex sex) {
+        if (isNotInit) {
+            initList();
+            isNotInit = false;
+        }
+
+        if (sex.isMan(sex)) {
+            return getNameFromList(LIST_WITH_MEAN_NAMES);
+        } else {
+            return getNameFromList(LIST_WITH_FEMALE_NAMES);
+        }
     }
 
     private List<Person> launchGeneratorPerson(int countPerson) {
